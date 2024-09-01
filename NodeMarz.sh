e25a52a54e69a08943b4799c29daeab5bbcaa6e4
Argo160 <116899376+Argo160@users.noreply.github.com>
2024-09-01 12:42:15 +0330

    Update NodeMarz.sh

--- NodeMarz.sh
+++ NodeMarz.sh
@@ -15,31 +15,36 @@ function main_menu {
     pp_lowercase=$(echo "$pp" | tr '[:upper:]' '[:lower:]')
     # Check if the input is "y"
     if [ "$pp_lowercase" = "y" ]; then
-      apt-get update && apt-get upgrade -y && apt-get install -y curl socat unzip git
-      echo -e "\e[32mSystem Updated and Upgraded and Curl+Socat+git+Unzip Installed.\e[0m"  # Green color for UP
-      curl -fsSL https://raw.githubusercontent.com/manageitir/docker/main/install-ubuntu.sh | sh
-      echo -e "\e[32mDocker Installed Successfully.\e[0m"  # Green color for UP
-      git clone https://github.com/Gozargah/Marzban-node
-      mkdir /var/lib/marzban-node
-      echo -e "\e[32mMarzban-Node Cloned Successfully.\e[0m"  # Green color for UP
-      sleep 5
-      clear
-      echo -e "\e[32mDo you wish to installe xray version 1.8.20? (y/n)\e[0m"  # Green color for UP
-      read -p "Y = Version 1.8.20 | N = Current Version: " ver
-      # Convert input to lowercase
-      ver_lowercase=$(echo "$ver" | tr '[:upper:]' '[:lower:]')
-      # Check if the input is "y"
+        apt-get update && apt-get upgrade -y && apt-get install -y curl socat unzip git
+        echo -e "\e[32mSystem Updated and Upgraded and Curl+Socat+git+Unzip Installed.\e[0m"  # Green color for UP
+        curl -fsSL https://raw.githubusercontent.com/manageitir/docker/main/install-ubuntu.sh | sh
+        echo -e "\e[32mDocker Installed Successfully.\e[0m"  # Green color for UP
+        git clone https://github.com/Gozargah/Marzban-node
+        mkdir /var/lib/marzban-node
+        echo -e "\e[32mMarzban-Node Cloned Successfully.\e[0m"  # Green color for UP
+        sleep 5
+        clear
+        echo -e "\e[32mDo you wish to installe xray version 1.8.20? (y/n)\e[0m"  # Green color for UP
+        read -p "Y = Version 1.8.20 | N = Current Version: " ver
+        # Convert input to lowercase
+        ver_lowercase=$(echo "$ver" | tr '[:upper:]' '[:lower:]')
+        # Check if the input is "y"
         if [ "$ver_lowercase" = "y" ]; then
-          cd
-          mkdir -p /var/lib/marzban/xray-core && cd /var/lib/marzban/xray-core
-          wget https://github.com/XTLS/Xray-core/releases/download/v1.8.20/Xray-linux-64.zip
-          unzip Xray-linux-64.zip
-          rm Xray-linux-64.zip
-
+            cd
+            mkdir -p /var/lib/marzban/xray-core && cd /var/lib/marzban/xray-core
+            wget https://github.com/XTLS/Xray-core/releases/download/v1.8.20/Xray-linux-64.zip
+            unzip Xray-linux-64.zip
+            rm Xray-linux-64.zip
+            cd ~/Marzban-node
+            curl -o docker-compose.yml https://raw.githubusercontent.com/Argo160/Marzban/main/doc1.8.20.yml
+        else
+            cd ~/Marzban-node
+            curl -o docker-compose.yml https://raw.githubusercontent.com/Argo160/Marzban/main/docCurrent.yml
+        fi
+        
 
           
-      cd ~/Marzban-node
-      nano docker-compose.yml
+ 
       read -n 1 -s -r -p "Press any key to continue"
       echo
     fi
